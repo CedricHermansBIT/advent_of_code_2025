7336b402f5b2fbd8116acd5457bfb7d939ea2257
CedricHermansBIT <cedric.hermans2@howest.be>
2025-12-07 00:20:22 +0100

    Update to make problem 2 solve way faster

--- day1/day1.py
+++ day1/day1.py
@@ -21,23 +21,26 @@ s=time()
 r=0
 x=50
 with open("input.txt") as ifile:
+    prev_null=False
     for line in ifile:
         d=line[0]
         n=int(line[1:])
+        r+=n//100
+        n%=100
         match d:
             case "L":
-                while n!=0:
-                    n-=1
-                    x-=1
-                    x%=100
-                    if x==0:
-                        r+=1
+                x-=n
+                if x<0 and not prev_null:
+                    r+=1
             case "R":
-                while n!=0:
-                    n-=1
-                    x+=1
-                    x%=100
-                    if x==0:
-                        r+=1
+                x+=n
+                if x>100:
+                    r+=1
+        x%=100
+        if x==0:
+            r+=1
+            prev_null=True
+        else:
+            prev_null=False
 print("p2: ",r)
 print(time()-s)
